b97132d9d848fe4bc3bce99136a14ae350f0b917
niquole <nicole.antonopoulou@gmail.com>
2019-07-05 02:44:03 +0200

    added new imaegs

--- src/views/maybeorder.vue
+++ src/views/maybeorder.vue
@@ -14,11 +14,12 @@
 
     <v-stepper-items>
       <v-stepper-content step="1">
-        <v-card > <div v-for="(dought,i) in doughts" v-bind:key="i" id="fill">
-        <v-btn @click="total(i)"> {{ dought.name }}  </v-btn>
+        <v-card > 
+            <div v-for="(dough,i) in this.donuts.dough" v-bind:key="i" id="fill">
+            <v-btn @click="total(i)"> {{ dough.name }}  </v-btn>
+            <!-- <img v-for="image in images" v-bind:key="image" :src="dough.image"> -->
             </div>
-            <img id="dought" src="../assets/img/donut-base.png" alt="">
-            </v-card>
+        </v-card>
      <div>
          <h1>total: {{ price }} €</h1>
      </div>
@@ -35,7 +36,7 @@
           <v-card > <div v-for="(filling,i) in fillings" v-bind:key="i" id="fill" @click="outpout">
         <v-btn @click="total(i)"> {{ filling.name }}  </v-btn>
             </div>
-        <img id="filling" src="../assets/img/withchoco.png" alt="">
+        <img id="filling" src="http://jupiterdonuts.com/assets/images/donuts/filled-blueberry.png" alt="">
             </v-card>
      <div>
          <h1>total: {{ price }} €</h1>
@@ -54,7 +55,7 @@
        <v-card>
             <div v-for="(ingridient,i) in ingridients" v-bind:key="i" id="topings" @click="outpout">
         <v-btn @click="total(i)"> {{ ingridient.name }}  </v-btn>
-        <img id="topings" src="../assets/img/sprinkled.png" alt="">
+        <img id="topings" src="https://static1.squarespace.com/static/56d8a4ed04426221d4b69175/5b454b0588251b2395abd77f/5b454b05758d461d370a4c6b/1531269610591/BlueberryFilled.PNG" alt="">
         </div>
         </v-card>
 
@@ -75,84 +76,31 @@
 
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "Order",
     data() {
         return {
-             e1: 0,
+            e1: 0,
+            urlApi: "https://api.myjson.com/bins/hn9xb",
             price: 0,
-            doughts: [
-                {
-                name: "normal",
-                price: 0.60
-                },
-                {
-                name: "chocolate",
-                price: 0.80
-                },
-                 {
-                name: "Crunchy",
-                price: 0.80
-                }, 
-                {
-                    name: "Vanilla",
-                    price: 0.70
-                },
-                 {
-                    name: "Caramel",
-                    price: 0.65
-                }
-            ],
-            ingridients: [
-                {
-                    name: "Milk Chocolate",
-                    price: 0.50
-                },
-                {
-                    name: "White Chocolate",
-                    price: 0.50
-                },
-                {
-                    name: "Dark Chocolate",
-                    price: 0.60
-                },
-                {
-                    name: "Strawberries",
-                    price: 0.70
-                },
-                {
-                    name: "Caramel",
-                    price: 0.30
-                },
-            ],
-            fillings: [
-                {
-                    name: "Chocolate",
-                    price: 0.60
-                },
-                {
-                    name: "Caramel",
-                    price: 0.40
-                },
-                {
-                    name: "Coconut",
-                    price: 0.80
-                },
-                {
-                    name: "Cherry",
-                    price: 0.30
-                }
-            ]
+            donuts: []
         }
+    },
+    mounted() {
+      axios.get(this.urlApi).then(response => {
+              this.donuts = response.data;
+      })
     },
     methods: {
         total: function(a) {
             this.price += this.ingridients[a].price;
             this.price += this.fillings[a].price;
+
         },
         reset: function() {
             this.price = 0;
-            this.step="1";
         },
         outpout: function() {
             this.ingridients.name;
@@ -161,13 +109,15 @@ export default {
         buy: function() {
             this.e1 = 1;
             this.price = 0;
+
         }
     }
-}
+}   
 </script>
 
 
 <style lang="scss">
+
 div#ingri{
     display: inline-block;
     text-align: center;
@@ -178,7 +128,9 @@ div#ingri{
     color: #fff;
     margin-bottom: 100px;
 }
+
 }
+
 div#fill {
     display: inline-block;
 .v-btn {
@@ -199,6 +151,7 @@ div#topings {
 }
 div {
     text-align: center;
+
 }
 span.primary {
     background-color: #FD8682 !important;
---
+++ src/views/shop.vue
@@ -0,0 +1,202 @@
+<template>
+
+  <v-container >
+    <div  v-for="(product, j) in this.products.products" :key="j">
+        <div row class="woof"><h2>{{ j }}</h2></div>
+    <v-layout>
+
+      <v-flex  v-for="(item, i) in product" :key="i" row>
+
+        <v-card xs12 md3 xl3 >
+          <v-img
+            :src="image"
+            height="300px"
+          > 
+          </v-img>
+          <v-card-title primary-title>
+            <div class="headline txtColor" v-bind="title">{{ item.title }}</div>
+            <v-spacer></v-spacer>
+            <div class="headline txtColor" v-bind="price">{{ item.price }} &euro;</div> 
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn icon @click="show = !show">
+              <v-icon color="#5d4383">{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn flat color="#5d4383" @click="something(item)">Add to cart</v-btn>
+          </v-card-actions>
+
+          <v-slide-y-transition>
+            <v-card-text v-show="show" >
+              <p v-bind="des">{{ item.des }}</p>
+              <p v-bind="weight">Weight (kg): {{ item.weight }}</p>
+              <p v-bind="material">Material: {{ item.material }}</p>
+            </v-card-text>
+          </v-slide-y-transition>
+        </v-card>
+
+      </v-flex>
+
+    </v-layout> 
+    <div row class="woof"></div>
+    </div>
+
+
+    <div class="cartOpen cart"  id="swoosh">
+        <div class="paw magic">
+            <v-icon @click="move()" class="btn" large color="#dcc7c6">pets</v-icon>
+        </div>
+        <div class="cartList magic" >
+            <div class="list" v-for="(item,k) in cart" :key="k">    
+              <div class="uno">{{ item.title }}</div>
+              <div class="dos">{{ item.price }}</div>
+              <div class="tres" @click="remove(k) ">x</div>             
+            </div>
+            <div class="total" v-bind="total">Total: {{ total }} &euro;</div>
+            <v-btn id="checky" class="total displayNo " dark color="#241f2b">Checkout</v-btn>
+        </div>
+    </div>
+  </v-container>
+
+</template>
+
+<style lang="scss">
+
+// CART SLIDER
+.cart {
+    
+    min-height: 400px;
+    width: 300px;
+    position: absolute;
+    right:0;
+    top: 100px;
+}
+.magic {
+    display: inline-block;
+}
+.paw {
+    border: 5px solid #251f2b;
+    border-radius: 50%;
+    width: 47px;
+    height: 47px;
+    background-color:#241f2b;
+    position: absolute;
+    left: -15px;
+    top: -15px;
+}
+.cartOpen {
+    transform: translate(250px);
+    transition: .5s;
+    position:fixed;
+}
+.cartClose {
+    transform: translate(0);
+    background-color: rgba(68, 58, 66, .9);
+    transition: .5s;
+    border-radius: 5px;
+    position:fixed;
+}
+.cartList {
+  margin: 0 0 0 50px;
+  min-height:350px;
+}
+.list > * {
+    display: inline-block;
+    color: #dcc7c6;
+    text-align: center;
+    padding: 15px;
+}
+.uno {
+  width: 65%;
+  text-align: left;
+}
+.dos {
+  width: 25%;
+}
+.tres{
+  width: auto;
+  padding: 5px;
+  cursor: pointer;
+  font-size: 18px;
+}
+.total {
+  color: #dcc7c6;
+  font-size: 20px;
+  font-weight: 700;
+  margin: 0 0 0 50px;
+}
+//cart button
+#checky {
+  font-weight: normal;
+  font-size: 14px; 
+}
+.displayNo {
+  display: none;
+}
+.displayYe {
+  display: inline-block;
+};
+</style>
+
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            show: false,
+            image: "https://cdn.pixabay.com/photo/2016/01/25/10/47/smiling-1160445_960_720.jpg",    
+            urlApi: "https://api.myjson.com/bins/hn9xb",
+            cart: [],
+            products: {},
+            total: 0  
+        }
+    },
+    mounted() {
+      axios.get(this.urlApi)
+          .then(response => {
+              this.products = response.data;
+      })
+        var cartOpen = agCookie.read('cart');
+        if( cartOpen == null || cartOpen === "closed") {
+            document.getElementById('swoosh').classList.add('cartOpen');
+        } else {
+            document.getElementById('swoosh').classList.add('cartClose');
+        }
+    },    
+    methods: {
+        something: function(item){
+          
+          this.total+= Number(item.price);
+          this.cart.push({title: item.title, price: item.price});
+          
+          if (this.cart == "") {
+          document.getElementById('checky').classList.add('displayNo');
+          document.getElementById('checky').classList.remove('displayYe');
+          }
+          if (this.cart != "") {
+          document.getElementById('checky').classList.add('displayYe');
+          document.getElementById('checky').classList.remove('displayNo');
+          }
+        },
+        move: function() {
+          var cartOpen = agCookie.read('cart');
+          if (cartOpen == null || cartOpen === "closed") {
+            document.getElementById('swoosh').classList.add('cartClose');
+            document.getElementById('swoosh').classList.remove('cartOpen');
+            agCookie.create('cart', 'opened');
+          }else {
+            document.getElementById('swoosh').classList.add('cartOpen');
+            document.getElementById('swoosh').classList.remove('cartClose');
+            agCookie.create('cart', 'closed');
+          }      
+        },
+        remove: function (k){
+          this.total -= Number(this.cart[k].price);
+          this.cart.splice(k, 1);       
+           
+        },
+    }
+}
+</script>
